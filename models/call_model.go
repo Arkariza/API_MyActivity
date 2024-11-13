b62d8c56136b32917f011eeebb0c39ff5a38cb10
InsaNation <Diantipsanaura@gmail.com>
2024-11-13 09:09:32 +0700

    model for call

---
+++ models/call_model.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Call struct {
+    ID             int       `json:"id" gorm:"primaryKey;autoIncrement"`
+    ClientName     string    `json:"client_name" gorm:"type:varchar(255)"`
+    Numphone       int       `json:"numphone"`
+    ProspectStatus string    `json:"prospect_status" gorm:"type:varchar(255)"`
+    Date           time.Time `json:"date"`
+    Note           *string   `json:"note" gorm:"type:text"`
+    CallResult     string    `json:"call_result" gorm:"type:text"`
+}
